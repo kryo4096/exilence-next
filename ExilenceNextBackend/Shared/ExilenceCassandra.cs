39abac3ed63910ffe0a45506df367110e8286726
Petter Annerwall <petterannerwall@gmail.com>
2020-02-10 22:57:24 +0100

    Commented UDT's

--- ExilenceNextBackend/Shared/ExilenceCassandra.cs
+++ ExilenceNextBackend/Shared/ExilenceCassandra.cs
@@ -15,13 +15,13 @@ namespace Shared
         {
             var cluster = Cluster.Builder().WithCredentials(username, password).AddContactPoints(hosts).Build();
 
-            Session.UserDefinedTypes.Define(UdtMap.For<Account>());
-            Session.UserDefinedTypes.Define(UdtMap.For<Connection>());
-            Session.UserDefinedTypes.Define(UdtMap.For<Group>());
-            Session.UserDefinedTypes.Define(UdtMap.For<SnapshotProfile>());
-            Session.UserDefinedTypes.Define(UdtMap.For<Snapshot>());
-            Session.UserDefinedTypes.Define(UdtMap.For<Stashtab>());
-            Session.UserDefinedTypes.Define(UdtMap.For<PricedItem>());
+            //Session.UserDefinedTypes.Define(UdtMap.For<Account>());
+            //Session.UserDefinedTypes.Define(UdtMap.For<Connection>());
+            //Session.UserDefinedTypes.Define(UdtMap.For<Group>());
+            //Session.UserDefinedTypes.Define(UdtMap.For<SnapshotProfile>());
+            //Session.UserDefinedTypes.Define(UdtMap.For<Snapshot>());
+            //Session.UserDefinedTypes.Define(UdtMap.For<Stashtab>());
+            //Session.UserDefinedTypes.Define(UdtMap.For<PricedItem>());
 
             Session = await cluster.ConnectAsync(keyspace);
 
